f33279c5228e44940a23087e741fb4854f8a128b
DewiSuryaMaulana <dewisuryamaulana11@gmail.com>
2023-10-24 05:59:35 +0700

    pert2, menggunakan mvc, contoh 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,16 +1,16 @@
-<?php
 
+<?php
 class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Nama saya Dewi Surya Maulana 
-        Saya Tinggal di Telaga Mas";
+        echo "Selamat datang, selamat belajar web proramming";
     }
 
     public function penjumlahan($n1, $n2) {
         $this->load->model('Model_latihan1');
         $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari $n1 + $n2 adalah $hasil";
+
+        $this->load->view('view-latihan', compact('hasil', 'n1', 'n2'));
     }
 }
